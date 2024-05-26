9c70bf5c9dd4e383c1caebba08fead94d0202373
Marcus Longmuir <marcus@mpl.dev>
2024-05-26 16:14:55 +0100

    Updated dependencies

--- mml-document/src/Light.tsx
+++ mml-document/src/Light.tsx
@@ -3,14 +3,14 @@ import React from "react";
 export default function Light() {
   return (
     <m-light
-      type="spotlight"
-      intensity="0.8"
-      x="5"
+      type="point"
+      intensity="500"
+      x="10"
       y="10"
-      z="5"
+      z="10"
       rx="10"
       rz="-10"
-      angle="90"
+      debug={true}
     ></m-light>
   );
 }
